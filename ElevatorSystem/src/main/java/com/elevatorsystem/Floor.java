bcf0fa2400eb7534e249f836d04c08a5d1b1f8cc
natborks <nathanael@moonsquare.co>
2025-06-03 23:42:31 -0230

    remove boundary check from floor class

--- ElevatorSystem/src/main/java/com/elevatorsystem/Floor.java
+++ ElevatorSystem/src/main/java/com/elevatorsystem/Floor.java
@@ -7,9 +7,7 @@ public abstract class Floor {
     public abstract int number();
 
     public static Floor create(int number) {
-        if (number < 1 || number > 100 || number == 13) {
-            throw new IllegalArgumentException("Invalid floor number");
-        }
+
         return new AutoValue_Floor(number);
     }
 
